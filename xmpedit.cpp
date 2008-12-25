78a64d22dad8f37694818152eba49319d89b1ba6
Dan Callaghan <djc@djc.id.au>
2008-12-25 17:01:01 +1000

    proper arg handling; avoid SIGABRT on exiv2 errors

--- xmpedit.cpp
+++ xmpedit.cpp
@@ -1,11 +1,29 @@
 
 #include <gtkmm/main.h>
+#include <glibmm/optioncontext.h>
 
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
-    Gtk::Main kit(argc, argv);
-    MainWindow window("/home/dan/Photos/indro3.jpg");
-    Gtk::Main::run(window);
+    Glib::OptionContext options("PHOTO_FILENAME");
+    try {
+        Gtk::Main kit(argc, argv, options);
+    } catch (Glib::OptionError e) {
+        std::cerr << argv[0] << ": option error: " << e.what() << std::endl;
+        exit(1);
+    }
+
+    if (argc < 2) {
+        std::cerr << argv[0] << ": no photo filename supplied" << std::endl;
+        exit(2);
+    }
+
+    try {
+        MainWindow window(argv[1]);
+        Gtk::Main::run(window);
+    } catch (Exiv2::Error e) {
+        std::cerr << argv[0] << ": exiv2 error: " << e.what() << std::endl;
+        exit(3);
+    }
     return 0;
 }
